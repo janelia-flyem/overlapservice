0f2dd02c22f3ecd64947ce6c406ff2eeddeeea80
Steve Plaza <plazas@janelia.hhmi.org>
2014-02-14 15:40:08 -0500

    cleans interaface more

--- overlap/formhtml.go
+++ overlap/formhtml.go
@@ -10,7 +10,7 @@ const formHTML=`
 </script>
 
 </head>
-
+<H2>Calculate Overlap Between a Set of Bodies</H2>
 <form id="overlapcalc" method="post">
 DVID server (e.g., emdata1:80): <input type="text" id="dvidserver" value="DEFAULT"><br>
 DVID uuid: <input type="text" id="uuid"><br>
@@ -26,6 +26,7 @@ Body list (e.g., 3, 4, 34): <input type="text" id="bodies"><br>
         <tr style='vertical-align: top'>
           <td style='width: 300px; font-size: 0.9em;'>
             <div id="control1"></div>
+            <div id="control2"></div>
           </td>
           <td style='width: 600px'>
             <div style="float: left;" id="chart1"></div>
@@ -35,6 +36,12 @@ Body list (e.g., 3, 4, 34): <input type="text" id="bodies"><br>
 </div>
 
 <script>
+    $("#control1").keydown(function() {
+        alert($("#control1").value()); 
+
+    });
+
+
     $("#overlapcalc").submit(function(event) {                                                           
       event.preventDefault();                                                                            
       $('#status').html("Processing....");
@@ -53,13 +60,12 @@ Body list (e.g., 3, 4, 34): <input type="text" id="bodies"><br>
             }
             $('#status').html("")
 
-            var column_names = ["body 1 - body 2", "overlap (# touching voxel faces)"];
+            var column_names = ["Body 1", "Body 2", "overlap (# touching voxel faces)"];
             var data_rows = [column_names];
 
             for (var i in results) {
                     var result_obj = results[i]
-                    var bodypair = result_obj[0].toString() + " - " + result_obj[1].toString();
-                    var rowval = [bodypair, result_obj[2]];
+                    var rowval = [result_obj[0], result_obj[1], result_obj[2]];
                     data_rows.push(rowval);
             }
 
@@ -67,12 +73,27 @@ Body list (e.g., 3, 4, 34): <input type="text" id="bodies"><br>
             // Prepare the data.
             var data = google.visualization.arrayToDataTable(data_rows);
           
-            // Define a StringFilter control for the 'body 1 - body 2' column
+            // Define a StringFilter control for the 'bodyid' column
             var stringFilter = new google.visualization.ControlWrapper({
               'controlType': 'StringFilter',
               'containerId': 'control1',
               'options': {
-                'filterColumnLabel': 'body 1 - body 2'
+                'filterColumnLabel': 'Body 1',
+                'ui' : {
+                    'label': 'Body 1 Filter'
+                }
+              }
+            });
+
+            // Define a StringFilter control for the 'bodyid' column
+            var stringFilter2 = new google.visualization.ControlWrapper({
+              'controlType': 'StringFilter',
+              'containerId': 'control2',
+              'options': {
+                'filterColumnLabel': 'Body 2',
+                'ui' : {
+                    'label': 'Body 2 Filter'
+                }
               }
             });
           
@@ -80,13 +101,14 @@ Body list (e.g., 3, 4, 34): <input type="text" id="bodies"><br>
             var table = new google.visualization.ChartWrapper({
               'chartType': 'Table',
               'containerId': 'chart1',
-              'options': {'height': '13em', 'width': '20em'}
+              'options': {'height': '13em', 'width': '25em'}
             });
           
             // Create the dashboard.
             var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard')).
               // Configure the string filter to affect the table contents
               bind(stringFilter, table).
+              bind(stringFilter2, table).
               // Draw the dashboard
               draw(data);
 
